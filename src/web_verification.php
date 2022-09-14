cc1768353da20f891f6184012a2d78516ed276ef
Charly MI <charly_1310@outlook.com>
2022-09-14 14:04:34 -0500

    feat(web_verification.php): add its class and its methods

---
+++ src/web_verification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SumaVdidPkgWeb;
+
+include "services/popup.php";
+include "services/redirect.php";
+
+class WebVerification
+{
+    protected string $api_key;
+
+    public function __construct(string $api_key)
+    {
+        $this->api_key = $api_key;
+    }
+
+    public function getApiKey()
+    {
+        return $this->api_key;
+    }
+
+    public static function verifyIdentity(string $identifier, string $method = 'redirect')
+    {
+        $key = self::getApiKey();
+        if ($method === 'popup') {
+            popup($key, $identifier);
+        } else {
+            redirect($key, $identifier);
+        }
+    }
+}
