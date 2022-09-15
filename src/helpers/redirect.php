9fdefd818e5d1fd07149f0d058fb264decce6eb3
Charly MI <charly_1310@outlook.com>
2022-09-14 22:00:23 -0500

    feat(helpers): add folder instead single file

--- src/helpers/redirect.php
+++ src/helpers/redirect.php
@@ -1,23 +1,5 @@
 <?php
-include 'constants.php';
-
-function popup(string $api_key, string $identifier)
-{
-    global $front_url;
-
-    $json = array(
-        'apiKey' => $api_key,
-        'identifier' => $identifier,
-        'popup' => true,
-        'mobile' => false
-    );
-    $json_stringify = json_encode($json);
-    $json_base64 = base64_encode($json_stringify);
-    $url = $front_url . "?state=" . $json_base64;
-    $link = "<script>window.open(" . $url . ")</script>";
-
-    return $link;
-}
+include '../constants/index.php';
 
 function redirect(string $api_key, string $identifier)
 {
