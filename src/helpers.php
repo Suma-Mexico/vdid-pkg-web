37a4e62e44df2a97f2ef40128bc9a092129f2479
Charly MI <charly_1310@outlook.com>
2022-09-14 20:23:27 -0500

    feat: change constants and helpers into a single files

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,23 @@
 <?php
-include 'constants/index.php';
+include 'constants.php';
+
+function popup(string $api_key, string $identifier)
+{
+    global $front_url;
+
+    $json = array(
+        'apiKey' => $api_key,
+        'identifier' => $identifier,
+        'popup' => true,
+        'mobile' => false
+    );
+    $json_stringify = json_encode($json);
+    $json_base64 = base64_encode($json_stringify);
+    $url = $front_url . "?state=" . $json_base64;
+    $link = "<script>window.open(" . $url . ")</script>";
+
+    return $link;
+}
 
 function redirect(string $api_key, string $identifier)
 {
